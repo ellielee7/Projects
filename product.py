85bfaef47d594d4bfa3c5623dd4670b5e958ca11
Liz Lee <lizlee7171@gmail.com>
2017-06-07 19:09:04 -0700

    First commit

--- product.py
+++ product.py
@@ -1,22 +1,39 @@
 class Product(object):
-    def __init__(self, price, item, weight, brand, cost, status):
-        self.price = price
+    def __init__(self, item, weight, brand, cost, status):
         self.item = item
         self.weight = weight
         self.brand = brand
         self.cost = cost
-        self.status = status
+        self.status = "For Sale"
         return self
-    def displayInfo(self):
-        print "Price: $" + str(self.price)
-        print "Item: " + str(self.item)
-        print "Weight: " + str(self.weight)
-        print "Brand: " + str(self.brand)
-        print "Total Cost: " + str(self.cost)
-        print "This item is " + str(self.status)
+    def sell(self):
+        self.status = "Sold!"
+        return self
+    def tax(self, float):
+        total = self.cost + (float * self.cost)
+        print "Your total with the tax is ${}".format(total)
+        return self
+    def Return(self):
+        reason = ("> ")
+        if "defective" in reason:
+            self.status = "Defective"
+            self.cost = 0
+            print self
+        elif "box" in reason:
+            self.status = "Like New, For Sale"
+            print self
+        elif "open" in reason:
+            self.status = "Used"
+            self.cost = self.cost - (self.cost * .20)
+            print self
         return self
 
-    def changeStatus(self):
-        if item == sold:
-            self.status = sold
-            print "This item is sold"
+    def displayInfo(self):
+        print "Your item is {} {}".format(self.brand, self.item)
+        print "Weight: {} ".format(self.weight)
+        print "Total Cost: {} ".format(self.cost)
+        print "This item is {} ".format(self.status)
+        return self
+
+tennis_Shoes = Product("Tennis Shoes", 1, "Nike", 99.99, "For Sale")
+tennis_Shoes.Return().displayInfo()
